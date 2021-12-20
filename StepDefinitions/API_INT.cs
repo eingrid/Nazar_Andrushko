bc3b5a3790653f76985eb5e46c7567dfdeaccb90
Nazar Andrushko <zikzak282@gmail.com>
2021-12-20 19:03:43 +0200

    Made class for interactions with API, added tests for Uploading and Deleting file

---
+++ StepDefinitions/API_INT.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace WebAPI_2.StepDefinitions
+{
+    public class API_INT
+    {
+        private readonly RestClient clientGetMetadata = new RestClient("https://api.dropboxapi.com/2/files/get_metadata");
+        private readonly RestRequest requestGetMeta = new RestRequest(Method.POST);
+
+        private readonly RestClient clientDelete = new RestClient("https://api.dropboxapi.com/2/files/delete_v2");
+        private readonly RestRequest requestDelete = new RestRequest(Method.POST);
+
+        private readonly RestClient clientUpload = new RestClient("https://content.dropboxapi.com/2/files/upload");
+
+        private readonly RestRequest requestUpload = new RestRequest(Method.POST);
+
+
+        private IRestResponse getList_Folder_response(){
+            RestClient clientCheck = new RestClient("https://api.dropboxapi.com/2/files/list_folder");
+            RestRequest requestCheck = new RestRequest(Method.POST);
+            clientCheck.Timeout = -1;
+            requestCheck.AddHeader("Authorization", "Bearer sl.A-hmLfSfjVSiqdmy1BK1zb7Sb5zOKRnDaJO656BIRVq4QvOCZ68PBCjoubDmPAeG8KSabiXEkdLhxu_5K8AWidfzsLRDbLMbwziUGDXzU1TLTel0BCteFbII_mTG65X-a-bVPOOuPnX3");
+            requestCheck.AddHeader("Content-Type", "application/json");
+            var body = @"{" + "\n" +
+            @"    ""path"" : """"" + "\n" +
+            @"}";
+            requestCheck.AddParameter("application/json", body,  ParameterType.RequestBody);
+            return clientCheck.Execute(requestCheck);
+        }
+        public void setClientUpload()
+        {
+            clientUpload.Timeout = -1;
+        }
+
+        public void sendRequestToUploadFIle()
+        {
+            requestUpload.AddHeader("Content-Type", "application/octet-stream");
+            requestUpload.AddHeader("Dropbox-API-Arg", "{\"path\": \"/NewFile.txt\",\"mode\": \"add\",\"autorename\": true,\"mute\": false,\"strict_conflict\": false}");
+            requestUpload.AddHeader("Authorization", "Bearer sl.A-hmLfSfjVSiqdmy1BK1zb7Sb5zOKRnDaJO656BIRVq4QvOCZ68PBCjoubDmPAeG8KSabiXEkdLhxu_5K8AWidfzsLRDbLMbwziUGDXzU1TLTel0BCteFbII_mTG65X-a-bVPOOuPnX3    ");
+            IRestResponse response = clientUpload.Execute(requestUpload);
+
+        }
+
+        public void CheckFileWasUpploaded()
+        {
+            var response = getList_Folder_response();
+
+            if (response.Content.IndexOf("NewFile.txt") == -1 ){
+                throw new ArgumentException("File wasn't added");
+            };
+        }
+
+
+
+
+        public void setClientDelete()
+        {
+            clientDelete.Timeout = -1;
+        }
+
+
+        public void sendRequestToDeleteFile()
+        {
+            requestDelete.AddHeader("Authorization", "Bearer sl.A-hmLfSfjVSiqdmy1BK1zb7Sb5zOKRnDaJO656BIRVq4QvOCZ68PBCjoubDmPAeG8KSabiXEkdLhxu_5K8AWidfzsLRDbLMbwziUGDXzU1TLTel0BCteFbII_mTG65X-a-bVPOOuPnX3");
+            requestDelete.AddHeader("Content-Type", "application/json");
+            var body = @"{" + "\n" +
+            @"    ""path"": ""/NewFile.txt""" + "\n" +
+            @"}";
+            requestDelete.AddParameter("application/json", body,  ParameterType.RequestBody);
+            IRestResponse response = clientDelete.Execute(requestDelete);
+        }
+
+        public void CheckFileWasDeleted()
+        {
+         
+            var response = getList_Folder_response();
+
+            if (response.Content.IndexOf("NewFile.txt") != -1 ){
+                throw new ArgumentException("File wasn't deleted");
+            };
+        }
+
+
+        public void prepateToGetMetaData()
+        {
+            RestClient clientGetMetadata = new RestClient("https://api.dropboxapi.com/2/files/get_metadata");
+            clientGetMetadata.Timeout = -1;
+            RestRequest requestGetMeta = new RestRequest(Method.POST);
+        }
+
+        public string sendRequestToGetMetadata()
+        {
+            requestGetMeta.AddHeader("Authorization", "Bearer sl.A-hmLfSfjVSiqdmy1BK1zb7Sb5zOKRnDaJO656BIRVq4QvOCZ68PBCjoubDmPAeG8KSabiXEkdLhxu_5K8AWidfzsLRDbLMbwziUGDXzU1TLTel0BCteFbII_mTG65X-a-bVPOOuPnX3");
+            requestGetMeta.AddHeader("Content-Type", "application/json");
+            var body = @"{" + "\n" +
+            @"    ""path"": ""/papka""" + "\n" +
+            @"}";
+            requestGetMeta.AddParameter("application/json", body,  ParameterType.RequestBody);
+            IRestResponse response = clientGetMetadata.Execute(requestGetMeta);
+
+            return response.Content;
+        }
+
+        public void checkGetMetadata(string metadata_response)
+        {
+            Console.WriteLine(metadata_response);
+        }
+    }
+}
--- StepDefinitions/Web2StepDefinitions.cs
+++ StepDefinitions/Web2StepDefinitions.cs
@@ -1,93 +1,64 @@
-using RestSharp;
-
+using static WebAPI_2.StepDefinitions.API_INT;
 namespace WebAPI_2.StepDefinitions
 {
     [Binding]
     public sealed class Web2StepDefinitions
     {
-        private readonly RestClient clientGetMetadata = new RestClient("https://api.dropboxapi.com/2/files/get_metadata");
-        private readonly RestRequest requestGetMeta = new RestRequest(Method.POST);
-
-        private readonly RestClient clientDelete = new RestClient("https://api.dropboxapi.com/2/file_requests/delete");
-        private readonly RestRequest requestDelete = new RestRequest(Method.POST);
-
-        private readonly RestClient clientUpload = new RestClient("https://content.dropboxapi.com/2/files/upload");
         
-        private readonly RestRequest requestUpload = new RestRequest(Method.POST);
-
+        private string metadata_response;
+        API_INT api_interactable = new API_INT(); 
         [Given(@"We have file to upload")]
         public void GivenWeHaveFileToUpload()
         {
-             clientUpload.Timeout = -1;
+             api_interactable.setClientUpload();
         }
 
         [When(@"We send a request to upload file")]
         public void WhenWeSendARequestToUploadFile()
         {
-            requestUpload.AddHeader("Dropbox-API-Arg", "{\\\"path\\\": \\\"/New.txt\\\",\\\"mode\\\": \\\"add\\\",\\\"autorename\\\": true,\\\"mute\\\": false,\\\"strict_conflict\\\": false}");
-            requestUpload.AddHeader("Authorization", "Bearer sl.A-Zzuh4QNNfZ_Ukqc_Qz1sV4Are7yMdSUt5oxoQJSpYhTMd7S9JoUCUcXutolccbUVfY8CcfxE0iLe2EQ3duNB1yic235FJpgpVzSU7L7Yek-jBmqRWMMB4VjHQGob7YCfY0qX6vdvMK");
-            requestUpload.AddHeader("Content-Type", "application/json");
-            var body = @"{" + "\n" +
-            @"    ""Content-Type"" : ""application/octet-stream""" + "\n" +
-            @"}";
-            requestUpload.AddParameter("application/json", body, ParameterType.RequestBody);
+            api_interactable.sendRequestToUploadFIle();
         }
 
         [Then(@"File is uploaded to dropbox")]
         public void ThenFileIsUploadedToDropbox()
         {
-            IRestResponse response = clientUpload.Execute(requestUpload);
-            Console.WriteLine(response.Content);
+            api_interactable.CheckFileWasUpploaded();
         }
 
         [Given(@"We have file to delete")]
         public void GivenWeHaveFileToDelete()
         {
-            clientDelete.Timeout = -1;
+            api_interactable.setClientDelete();
         }
 
         [When(@"We send a request to delete file")]
         public void WhenWeSendARequestToDeleteFile()
         {
-            requestDelete.AddHeader("Authorization", "Bearer sl.A-Zzuh4QNNfZ_Ukqc_Qz1sV4Are7yMdSUt5oxoQJSpYhTMd7S9JoUCUcXutolccbUVfY8CcfxE0iLe2EQ3duNB1yic235FJpgpVzSU7L7Yek-jBmqRWMMB4VjHQGob7YCfY0qX6vdvMK");
-            requestDelete.AddHeader("Content-Type", "application/json");
-            var body = @"{" + "\n" +
-            @"    ""path"": ""Document.docx""" + "\n" +
-            @"}";
-            requestDelete.AddParameter("application/json", body, ParameterType.RequestBody);
+            api_interactable.sendRequestToDeleteFile();
         }
 
         [Then(@"File is deleted on dropbox")]
         public void ThenFileIsDeletedOnDropbox()
         {
-            IRestResponse response = clientDelete.Execute(requestDelete);
-            Console.WriteLine(response.Content);
+            api_interactable.CheckFileWasDeleted();
         }
 
         [Given(@"We want to get metadata of the file on dropbox")]
         public void GivenWeWantToGetMetadataOfTheFileOnDropbox()
         {
-            RestClient clientGetMetadata = new RestClient("https://api.dropboxapi.com/2/files/get_metadata");
-            clientGetMetadata.Timeout = -1;
-            RestRequest requestGetMeta = new RestRequest(Method.POST);
+            api_interactable.prepateToGetMetaData();
     }
 
         [When(@"We send a request to get metadata")]
         public void WhenWeSendARequestToGetMetadata()
         {
-            requestGetMeta.AddHeader("Authorization", "Bearer sl.A-Zzuh4QNNfZ_Ukqc_Qz1sV4Are7yMdSUt5oxoQJSpYhTMd7S9JoUCUcXutolccbUVfY8CcfxE0iLe2EQ3duNB1yic235FJpgpVzSU7L7Yek-jBmqRWMMB4VjHQGob7YCfY0qX6vdvMK");
-            requestGetMeta.AddHeader("Content-Type", "application/json");
-            var body = @"{" + "\n" +
-            @"    ""path"": ""/New.txt""" + "\n" +
-            @"}";
-            requestGetMeta.AddParameter("application/json", body, ParameterType.RequestBody);
+            metadata_response = api_interactable.sendRequestToGetMetadata();
         }
 
         [Then(@"We get metadata")]
         public void ThenWeGetMetadata()
         {
-            IRestResponse response = clientGetMetadata.Execute(requestGetMeta);
-            Console.WriteLine(response.Content);
+            api_interactable.checkGetMetadata(metadata_response);
         }
 
     }
